a61efbfaaec9bbdb2acd951c2d3711061b09ff40
Glauco Russo | Full-Stack Web Engineer <glauco.developer@gmail.com>
2025-01-16 06:48:59 -0300

    remove comment

--- db/search-replace.sql
+++ db/search-replace.sql
@@ -1,6 +1,5 @@
 USE wordpress;
 
--- Realize o search & replace nas URLs
 UPDATE ety39_options SET option_value = REPLACE(option_value, 'https://www.kerrygoldusa.com', 'http://localhost:8080') WHERE option_name IN ('home', 'siteurl');
 UPDATE ety39_posts SET guid = REPLACE(guid, 'https://www.kerrygoldusa.com', 'http://localhost:8080');
 UPDATE ety39_posts SET post_content = REPLACE(post_content, 'https://www.kerrygoldusa.com', 'http://localhost:8080');
